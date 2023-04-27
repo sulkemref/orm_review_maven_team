a0c1a3debecc5411d098cdb871a48385e5d70720
Kemail-terminal <sulkemref@gmail.com>
2023-04-27 10:01:00 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/orm_review_maven_team/OrmReviewMavenTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.orm_review_maven_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmReviewMavenTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
